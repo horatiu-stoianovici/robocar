d6f728cc9bd90f40450621fbe3ad40b73d8bdddd
Horatiu Stoianovici <horatiu.stoianovici@gmail.com>
2015-04-08 17:39:20 +0000

    First working version for network control server

--- communications.py
+++ communications.py
@@ -1,18 +1,65 @@
 import socket
-import _thread as thread
+import thread
+from carcontrol import CarController
 
 TCP_PORT = 5005
 BUFFER_SIZE = 3
+controller = CarController()
 
+#this is the main loop for receiving data
 def __mainLoop(s):
+        global controller
 	conn, addr = s.accept()
+
+	#after connection is accepted start a new thread that will be able to accept a new connection
+	thread.start_new_thread(__mainLoop, (s,))
 	print ('Connected from ', addr)
+	#read data while connection is live
 	while 1:
 		data = conn.recv(BUFFER_SIZE)
 		if not data: 
 			break
-		print('received data:', data)
-		conn.send(data.upper())
+		if data[0] == 'L':
+                        #left
+                        if data[1] == 'D':
+                                #key down
+                                controller.startLeft()
+                                pass
+                        elif data[1] == 'U':
+                                #key up
+                                controller.stopLeft()
+                                pass
+                elif data[0] == 'R':
+                        #right
+                        if data[1] == 'D':
+                                #key down
+                                controller.startRight()
+                                pass
+                        elif data[1] == 'U':
+                                #key up
+                                controller.stopRight()
+                                pass
+                elif data[0] == 'F':
+                        #forward
+                        if data[1] == 'D':
+                                #key down
+                                controller.startForward()
+                                pass
+                        elif data[1] == 'U':
+                                #key up
+                                controller.stopForward()
+                                pass
+                elif data[0] == 'B':
+                        #backward
+                        if data[1] == 'D':
+                                #key down
+                                controller.startBackward()
+                                pass
+                        elif data[1] == 'U':
+                                #key up
+                                controller.stopBackward()
+                                pass
+		conn.send('OK')
 	conn.close()
 
 def startServer():
@@ -26,4 +73,5 @@ def startServer():
 
 
 startServer()
-input()
+raw_input()
+controller.__exit__(None, None, None)
